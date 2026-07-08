0514d470e2cd278b6457dae7328a56f8a9096851
Dhruv Saxena <saxenadhruv273@gmail.com>
2026-07-08 15:38:13 +0530

    Updation

---
+++ Question015.java
@@ -0,0 +1,29 @@
+
+// Code
+// Testcase
+// Testcase
+// Test Result
+// 9. Palindrome Number
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
