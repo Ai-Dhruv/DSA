413ffea235d2a35e614b67fc816ae65b4fcd2890
Dhruv Saxena <saxenadhruv273@gmail.com>
2026-08-30 22:16:23 +0530

    Solve Question063

---
+++ Question063.java
@@ -0,0 +1,23 @@
+// 1470. Shuffle the Array
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int j = 0;
+
+        for(int i=0; i<n; i++){
+            ans[j++] = nums[i];
+            ans[j++] = nums[i+n];
+        }
+        return ans;
+    }
+}
